562a1df16a7af7cd756dc2eceba9dc6513caa532
Howard <howard@TSC1s-MacBook-Pro.local>
2018-04-08 16:28:15 -0400

    Got checkall gui function working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow,QWidget,QTableWidget,QTableWidgetItem,QPushButton,QPlainTextEdit, QDialog, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow,QWidget,QTableWidget,QTableWidgetItem,QPushButton,QPlainTextEdit, QDialog, QComboBox, QCheckBox
 from dialog import Ui_Dialog
 from PyQt5.QtCore import pyqtSlot
 
@@ -19,17 +19,25 @@ class Dialog(QDialog):
         self.ok_button.clicked.connect(self.accept)
         self.cancel_button.clicked.connect(self.reject)
         self.apply_button.clicked.connect(self.apply_changes)
+
+        self.all_chk = self.get_child_widget(self.ui.days_available_grpbox,QCheckBox,"all_days_chk")
+        self.all_chk.clicked.connect(self.check_all_days)
+    
         
 
         
     @pyqtSlot()
     def apply_changes(self):
-        self.get_category()
+        self.get_all_options()
         
    
     def get_all_options(self):
-        pass
-    
+        self.get_location()
+        self.get_category()
+        self.get_professor()
+        self.get_days()
+        
+
     def get_time_available(self):
         pass
     def get_time_from(self):
@@ -43,11 +51,57 @@ class Dialog(QDialog):
         print(self.get_cwtext(self.ui.category_grpbox,QComboBox,"category_comb"))
 
     def get_location(self):
-        pass
+        print(self.get_cwtext(self.ui.location_grpbox,QComboBox,"location_comb"))
+        
     
     def get_days(self):
-        pass
+        
+        print(self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"all_days_chk"))
+        print(self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"monday_chk"))
+        print(self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"tues_chk"))
+        print(self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"wed_chk"))
+        print(self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"thurs_chk"))
+        print(self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"fri_chk"))
     
+    @pyqtSlot()
+    def check_all_days(self):
+        all_is_checked = self.get_checkbox_helper(self.ui.days_available_grpbox,QCheckBox,"all_days_chk")
+        if all_is_checked:
+            self.check_rest()
+        
+    def check_rest(self):
+        monday = self.get_child_widget(self.ui.days_available_grpbox,QCheckBox,"monday_chk")
+        tuesday = self.get_child_widget(self.ui.days_available_grpbox,QCheckBox,"tues_chk")
+        wednesday = self.get_child_widget(self.ui.days_available_grpbox,QCheckBox,"wed_chk")
+        thursday = self.get_child_widget(self.ui.days_available_grpbox,QCheckBox,"thurs_chk")
+        friday = self.get_child_widget(self.ui.days_available_grpbox,QCheckBox,"fri_chk")
+        
+        monday.setChecked(True)
+        tuesday.setChecked(True)
+        wednesday.setChecked(True)
+        thursday.setChecked(True)
+        friday.setChecked(True)
+        
+        #immediately update gui to reflect changes
+        #note: choice of monday is arbitrary
+        monday.repaint()
+        
+            
+
+    def get_child_widget(self,parent,child_widget_type,child_name):
+        child = parent.findChild(child_widget_type,child_name)
+        return child
+
+        
+
+    def get_checkbox_helper(self,parent,child_widget_type,child_name):
+        child = parent.findChild(child_widget_type,child_name)
+        child_is_checked = child.isChecked()
+        return child_is_checked
+
+    def get_professor(self):
+        print(self.get_cwtext(self.ui.professor_grpbox,QComboBox,"professor_comb"))
+
     def get_cwtext(self,parent,child_widget_type,child_name):
         child = parent.findChild(child_widget_type,child_name)
         child_text = str(child.currentText())
